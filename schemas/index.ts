35e07fbcb4527bfe70bd18dc4904a7f2705a54fa
Atul <atulmalviyav2@gmail.com>
2024-09-23 20:09:05 +0530

    register server actions and data validation

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,3 +5,9 @@ export const  LoginSchema = Z.object({
     password: Z.string().min(4, { message: 'Password must be 4 char long' }),
 });
 
+export const  RegisterSchema = Z.object({
+    email: Z.string().email({ message: 'Please enter a valid email' }),
+    password: Z.string().min(4, { message: 'Password must be 4 char long' }),
+    name: Z.string().min(1, { message: 'Name is required' }),
+});
+
